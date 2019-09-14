0ed83ffe31e9c068e653c29dd7007b647fd20f8b
tonywinslow@yahoo.com <tonywinslow@yahoo.com>
2019-09-14 10:16:14 -0400

    Move custom fonts out of assesets pipeline and into webpacker.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,7 @@ import 'packs/tracks.coffee'
 
 // Custom Stylesheets.
 import 'src/application'
+require.context('../images', true)
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
